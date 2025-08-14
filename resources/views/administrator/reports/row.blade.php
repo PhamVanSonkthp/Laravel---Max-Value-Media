a38430ee46435a9f6b4effa8e699ccfda45351e8
Son <bontukyhpkt@gmail.com>
2025-08-14 15:43:31 +0700

    son

--- resources/views/administrator/reports/row.blade.php
+++ resources/views/administrator/reports/row.blade.php
@@ -1,25 +1,4 @@
 <tr class="" id="tr_container_index_{{$index}}" data-id="{{$item->id}}">
-    {{--    <td class="text-center">--}}
-    {{--        <input type="checkbox" class="checkbox-delete-item" value="{{$item->id}}">--}}
-    {{--    </td>--}}
-    {{--    <td>--}}
-    {{--        @include('administrator.components.sort_icon_for_table', ['prefixView' => $prefixView])--}}
-
-    {{--        {{$item->id}}--}}
-    {{--    </td>--}}
-    {{--    <td>{{$item->title ?? $item->name}}</td>--}}
-    {{--    <td>--}}
-    {{--        <img class="rounded-circle" src="{{$item->avatar()}}" alt="">--}}
-    {{--    </td>--}}
-    {{--    <td>--}}
-    {{--        {{ optional($item->createdBy)->name}}--}}
-    {{--    </td>--}}
-    {{--    <td>{{\App\Models\Formatter::getDateTime($item->created_at)}}</td>--}}
-    {{--    <td>--}}
-
-    {{--        @include('administrator.components.action_table', ['prefixView' => $prefixView, '$item' => $item])--}}
-    {{--    </td>--}}
-
     <td>
         {{\App\Models\Formatter::getOnlyDate($item->date)}}
     </td>
@@ -29,14 +8,11 @@
     <td>
         <div style="max-width: 200px;overflow: hidden;">
             <a target="_blank" href="{{ optional(optional($item->zoneWebsite)->website)->url}}">
-                {{ optional(optional($item->zoneWebsite)->website)->name}}
+                {{\App\Models\Formatter::maxLengthString(optional(optional($item->zoneWebsite)->website)->name)}}
             </a>
 
         </div>
     </td>
-{{--    <td>--}}
-{{--        {{$item->zone_website_id}}--}}
-{{--    </td>--}}
     <td>
         {{ optional($item->zoneWebsite)->name}}
     </td>
