d7d8304a54cb1fd7700379b4514b5027e24db9c6
Son <bontukyhpkt@gmail.com>
2025-08-22 16:09:26 +0700

    son

--- resources/views/user/components/slidebars.blade.php
+++ resources/views/user/components/slidebars.blade.php
@@ -13,7 +13,7 @@
         <ul class="nav">
             <li class="nav-item nav-category">Main</li>
             <li class="nav-item">
-                <a href="{{route('user.index')}}" class="nav-link">
+                <a href="{{route('user.dashboard')}}" class="nav-link">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                          class="feather feather-box link-icon">
