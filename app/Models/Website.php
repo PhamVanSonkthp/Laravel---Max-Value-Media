d0d309faeb4ce4cf91793aa68f4b89adb5286298
Son <bontukyhpkt@gmail.com>
2025-09-12 12:50:41 +0700

    son

--- app/Models/Website.php
+++ app/Models/Website.php
@@ -66,14 +66,14 @@ class Website extends Model implements Auditable
     public function getMaxDImpressionOneDay()
     {
 
-        $maxImpressionDay = DB::table('reports')
-            ->select('date', DB::raw('SUM(d_impression) as total_impression'))
-            ->groupBy('date')
+        $maxImpression = DB::table('reports')
+            ->select('website_id', 'date', DB::raw('SUM(d_impression) as total_impression'))
+            ->groupBy('website_id', 'date')
             ->orderByDesc('total_impression')
             ->first();
 
 
-        return optional($maxImpressionDay)->total_impression ?? 0;
+        return optional($maxImpression)->total_impression ?? 0;
     }
 
     public function getMaxRequestOneDay()
