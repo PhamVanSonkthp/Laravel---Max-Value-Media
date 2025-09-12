1296ea1f754fcc8d8fb4219e83b745c6e9389667
Son <bontukyhpkt@gmail.com>
2025-09-12 15:46:57 +0700

    son

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -40,18 +40,18 @@ class ReportController extends Controller
     public function index(Request $request)
     {
         $modelSummary = new Report();
-        $sumary = $modelSummary->searchByQuery($request, ['report_type_id' => 1], null, null, true);
+        $sumary = $modelSummary->searchByQuery($request, [], null, null, true);
 
         $sumary->selectRaw('SUM(d_request) as d_request, SUM(d_impression) as d_impression, SUM(d_impression_us_uk) as d_impression_us_uk, AVG(d_ecpm) as d_ecpm, SUM(d_revenue) as d_revenue, AVG(count) as count, AVG(share) as share, SUM(p_impression) as p_impression, AVG(p_ecpm) as p_ecpm, SUM(p_revenue) as p_revenue, SUM(profit) as profit, SUM(sale_percent) as sale_percent, SUM(system_percent) as system_percent, SUM(salary) as salary, SUM(deduction) as deduction, SUM(net_profit) as net_profit');
         $sumary = $sumary->first();
 
-        $modelAdserverSummary = new Report();
-        $adServerSumary = $modelAdserverSummary->searchByQuery($request, [], null, null, true);
-        $adServerSumary->selectRaw('SUM(d_request) as d_request');
-        $adServerSumary = $adServerSumary->first();
+//        $modelAdserverSummary = new Report();
+//        $adServerSumary = $modelAdserverSummary->searchByQuery($request, [], null, null, true);
+//        $adServerSumary->selectRaw('SUM(d_request) as d_request');
+//        $adServerSumary = $adServerSumary->first();
 
-        $sumary->d_request = 0;
-        $sumary->d_request += $adServerSumary->d_request;
+//        $sumary->d_request = 0;
+//        $sumary->d_request += $adServerSumary->d_request;
 
         $items = $this->model->searchByQuery($request, [], null, null, true);
 
